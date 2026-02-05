f3b4b430b733df23096a9a2faa0d51d10ed039eb
sonubhosle <sbhosle1011@gmail.com>
2026-02-05 20:31:55 +0530

    DEL

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const DbConnect = require('./config/db');
-
-DbConnect()
-const PORT = 6789
-
-app.listen(PORT , () => {
-    console.log(`http://localhost:${PORT}`)
-})
